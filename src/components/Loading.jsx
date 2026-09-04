d4d2fb1027a41946a33c8d61c93cdbed7ddde093
arifaslam2002 <aarifaslam2002@email.com>
2026-09-04 09:29:05 +0530

    Add Spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,11 @@
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center mt-10">
-      <p className="text-xl font-semibold">Loading...</p>
+    <div className="flex flex-col justify-center items-center mt-16 gap-4">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
+
+      <p className="text-lg font-semibold text-gray-700">
+        Searching for shows...
+      </p>
     </div>
   );
 };
