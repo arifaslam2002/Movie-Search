d2ba3baf506c587b45951ddd506c1e070b5ca26d
arifaslam2002 <aarifaslam2002@email.com>
2026-09-04 07:35:38 +0530

    Add Tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white" >MovieMania🎥</h1>
+    </div>
   )
 }
 
